279ce9c85361c0dc8edb54bbf5a362eb6d339e80
Keshav2207 <70221686+Keshav2207@users.noreply.github.com>
2025-09-02 11:36:17 +0000

    Deploying to gh-pages from @ Keshav2207/portfolio@4ec305f0c626c3bdc1894823b8db5b12e2c354ee 🚀

---
+++ _nuxt/CQt9s8jR.js
@@ -0,0 +1,1 @@
+import{r as t}from"./C0rnjoQh.js";import a from"./5E_Gb8dB.js";import{y as e,E as n,h as s,w as c,I as i}from"./DtohSkrs.js";import"./iik6CYzq.js";const d={__name:"ProseCaution",setup(p){const o=e();return(r,f)=>(s(),n(a,{color:"error",icon:i(o).ui.icons.caution},{default:c(()=>[t(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
